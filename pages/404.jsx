3c53a9e76d2fe4ff07d4a6d2c2e6c5f3c720eec0
berat5670 <140914021+berat5670@users.noreply.github.com>
2024-08-22 15:45:26 +0300

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -2,8 +2,8 @@ import ErrorPage from '../components/ErrorPage';
 const UnkownPage = () => {
     let messages = [
         'Bir Hatayla Karşılaştın',
-        'Hatalı Bir Komut Girdin!',
-        'BRT Pro | 404 Hata'
+        'Bir Hatayla Karşılaştın',
+        'Bir Hatayla Karşılaştın'
     ];
 
     Geri Dön <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Page not found."} />
